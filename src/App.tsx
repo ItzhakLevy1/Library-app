29fc310c9afbb97f41184377c915fafded3473cd
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-11-06 20:29:14 +0200

    Explore top books component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { ExplorTopBooks } from "./layouts/HomePage/ExploreTopBook";
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar />
+      <ExplorTopBooks />
+    </div>
   );
 }
 
