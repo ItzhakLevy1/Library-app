eace853bdd208baff5e9eeec28c555134daa7c82
ItzhakLevy1 <itzhaklevydev@gmail.com>
2024-11-08 16:22:18 +0200

    Add App.CSS and a Carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { ExplorTopBooks } from "./layouts/HomePage/ExploreTopBook";
+import { Carousel } from "./layouts/HomePage/Carousel";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExplorTopBooks />
+      <Carousel />
     </div>
   );
 }
