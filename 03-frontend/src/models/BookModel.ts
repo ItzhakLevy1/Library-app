caf55fee213ffab378737cf0caa1525c9057afcf
Itzhak Levy <itzhaklevydev@gmail.com>
2024-12-13 23:22:03 +0200

    Add BookModel.ts

---
+++ 03-frontend/src/models/BookModel.ts
@@ -0,0 +1,32 @@
+class BookModel {
+  id: number;
+  titls: string;
+  author: string;
+  description: string;
+  copies: number;
+  copiesAvailable?: number;
+  category?: string;
+  img?: string;
+
+  constructor(
+    id: number,
+    titls: string,
+    author: string,
+    description: string,
+    copies: number,
+    copiesAvailable?: number,
+    category?: string,
+    img?: string
+  ) {
+    this.id = id;
+    this.titls = titls;
+    this.author = author;
+    this.description = description;
+    this.copies = copies;
+    this.copiesAvailable = copiesAvailable;
+    this.category = category;
+    this.img = img;
+  }
+}
+
+export default BookModel;
